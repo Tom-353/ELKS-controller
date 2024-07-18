634572dd292b25c6c6d99ba967283df3a38e3162
Tomáš Řehák <tomrehak353@gmail.com>
2024-07-18 18:20:28 +0200

    - Removed unnecessary code  + Added connect request  + Added Readme

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,40 @@
 import { SmartLed, LED_WS2812, Rgb } from "smartled";
 import * as radio from "simpleradio";
-import * as colors from "./libs/colors.js"
 import * as adc from "adc";
 import * as gpio from "gpio";
 
 const BTN_LEFT = 18;
 const INPUT_PINS = [4,6,2,1];
-const ledStrip = new SmartLed(40, 10, LED_WS2812);
-const ledStrip2 = new SmartLed(21, 8, LED_WS2812);
-ledStrip.set(0, {r:0,g:20,b:0});
-ledStrip.show();
-radio.begin(4);
 INPUT_PINS.forEach(pin => {
     adc.configure(pin); 
 });
+
+var values: Array<number> = [0,0,0,0];
+
+const ledStrip = new SmartLed(48, 1, LED_WS2812);
+
+radio.begin(4);
 gpio.pinMode(BTN_LEFT, gpio.PinMode.INPUT);
 
 gpio.on("falling", BTN_LEFT, () => {
-    console.log("pressed");
-    radio.sendString("Hmmm");
+    radio.sendString("Connect");
+    console.log("Connect");
 });
-/*
-// řetězce
-radio.on("string", (retezec, info) => {
-    console.log(
-      `Přijal jsem řetězec '${retezec}'.
-      Od: ${info.address},
-      síla signálu: ${info.rssi})
-  `
-    );
-  });*/
-  /*
-// číslo
-radio.on("number", (cislo, info) => {
-    console.log(
-      `Přijal jsem číslo ${cislo}.
-      Od: ${info.address},
-      síla signálu: ${info.rssi})
-  `
-    );
-  });
-// klíč - hodnota
-radio.on("keyvalue", (klic, hodnota, info) => {
-    console.log(
-      `Přijal jsem ${klic} = ${hodnota}.
-      Od: ${info.address},
-      síla signálu: ${info.rssi})
-  `
-    );
-  });*/
+
 var t: number = 0;
-function adjustADC(inut: number) : number {
-  const out = 2*inut/1023 -1
-  return out-out%0.1;
-}
-setInterval(() => { // pravidelně vyvolává událost
+
+setInterval(() => { // pravidelně vysílá příkazy
+    // Blikání LEDky
     ledStrip.clear();
-    ledStrip.set(t%10, {r:0,g:20,b:0});
+    if (t%2 == 0) ledStrip.set(0, {r:0,g:20,b:0});
     ledStrip.show();
     t+=1;
-    const value = adc.read(INPUT_PINS[0]);
-    const value2 = adc.read(INPUT_PINS[1])
-    const value3 = adc.read(INPUT_PINS[2]);
-    const value4 = adc.read(INPUT_PINS[3])
-    const mystring = "P"+value+";"+value2+";"+value3+";"+value4;
-    console.log(mystring);
+    // Vysílání datr z potenciometrů
+    for (let i = 0; i < INPUT_PINS.length; i++) {
+      values[i] = adc.read(INPUT_PINS[i]);
+    }
+    values.toString();
+    const mystring = "P"+values.toString();;
     radio.sendString(mystring);
+    console.log(mystring);
 }, 100);
-setInterval(() => { // pravidelně vyvolává událost
-    const value = adc.read(INPUT_PINS[0]);
-    const value2 = adc.read(INPUT_PINS[1]);
-    //console.log(value*8/1024);
-    ledStrip2.clear();
-    ledStrip2.set(value*8/1024, {r:0,g:20,b:0});
-    ledStrip2.set(value2*8/1024, {r:20,g:0,b:0});
-    ledStrip2.show();
-    //radio.sendKeyValue("X", -999);
-    
-}, 50);
